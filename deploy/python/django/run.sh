efc1f79265c4169f4183f83af19ad0dfd67f0582
Karim-Valeev <karim.valeev.i@gmail.com>
2022-04-05 11:24:01 +0300

    Add nginx, grafana, loki and prod dc file

---
+++ deploy/python/django/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for PostgreSQL..."
+
+    while ! nc -z "$DB_HOST" "$DB_PORT"; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python src/manage.py migrate
+python src/manage.py collectstatic --noinput
+
+exec "$@"
